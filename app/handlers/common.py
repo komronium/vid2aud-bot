af1798ae18f1a97bbd9b3fab6fccf60b76bae674
komronium <komronium@gmail.com>
2025-01-27 12:23:26 +0500

    add stats

--- app/handlers/common.py
+++ app/handlers/common.py
@@ -20,9 +20,9 @@ async def get_stats(message: types.Message):
     top_5_users = await UserService().get_top_5_user()
 
     response_message1 = (
-        "`╒═════════════════════╤══════════╕\n"
-        "│ Platform Statistics │          │\n"
-        "╞═════════════════════╪══════════╡\n"
+        "`╒════════════════════════════════╕\n"
+        "│ Platform Statistics            │\n"
+        "╞═════════════════════╤══════════╡\n"
         f"│ Total Users         │ {user_count}{' ' * (8 - len(str(user_count)))} │\n"
         f"│ Users Joined Today  │ {today_joined_user_count}{' ' * (8 - len(str(today_joined_user_count)))} │\n"
         f"│ Total Conversions   │ {conversion_count}{' ' * (8 - len(str(conversion_count)))} │\n"
@@ -30,10 +30,16 @@ async def get_stats(message: types.Message):
     )
 
 
-    response_message2 = f"*Top 5 Users:*\n"
+    response_message2 = (
+        "`╒════════════════════════════════╕\n"
+        "│ Top 5 Users                    │\n"
+        "╞═════════════════════╤══════════╡\n"
+    )
 
     for idx, user in enumerate(top_5_users, start=1):
-        response_message2 += f"{idx}. {user.full_name} - {user.conversion_count} points\n"
+        response_message2 += f"│ {idx} │ {user.full_name[:20]} │ {user.conversion_count} │\n"
+
+    response_message2 +=  "╘═════════════════════╧══════════╛`"
 
     await message.answer(response_message1, parse_mode="Markdown")
     await message.answer(response_message2, parse_mode="Markdown")
